71202e63bd97be0254244705822c1fc6b2119494
Rashmi Raghu <rraghu@pivotal.io>
2017-01-09 13:48:27 -0800

    code to preprocess data; assess measurement intervals etc

--- src/02-pre-processing.sql
+++ src/02-pre-processing.sql
@@ -36,4 +36,46 @@ CREATE OR REPLACE VIEW mgdemo.mgdata_dt30min_check_view1 AS
 			building_num
 	) t2;
 
-select * from mgdemo.mgdata_dt30min_check_view1 order by building_num;
+-- select * from mgdemo.mgdata_dt30min_check_view1 order by building_num;
+
+select diff_ct, count(*) as ct_buildings
+from mgdemo.mgdata_dt30min_check_view1
+group by diff_ct
+order by diff_ct;
+
+/*
+ * VIEW with only those meters that have diff_ct = 1. This query gets those meters
+ * consistently collected data every 60 seconds.
+ */
+CREATE OR REPLACE VIEW mgdemo.mgdata_diffct1_view1 AS
+	SELECT t1.*
+	FROM
+		mgdemo.microgrid_data t1
+		, mgdemo.mgdata_dt30min_check_view1 t2
+	WHERE
+		t1.building_num = t2.building_num
+		AND t2.diff_ct = 1;
+
+select count(*) as ct_buildings
+from
+(
+	select building_num from mgdemo.mgdata_diffct1_view1
+	group by 1
+) t1;
+
+
+-- TABLE containing start and end times for each meter and COUNT of how many data points we have.
+CREATE TABLE mgdemo.mgdata_dfct1_itvlcts_t1 AS
+	SELECT *
+	FROM (
+		SELECT
+			building_num,
+			min(tslocal) AS ts_start,
+			max(tslocal) AS ts_end,
+			COUNT(tslocal) AS ct_itvl_ts
+		FROM
+			mgdemo.mgdata_diffct1_view1
+		GROUP BY
+			building_num
+		) t1
+		DISTRIBUTED BY (building_num);
