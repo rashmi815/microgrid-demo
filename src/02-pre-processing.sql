a364f55d26af73991f9e00752577d864f75407e8
Rashmi Raghu <rraghu@pivotal.io>
2017-01-15 19:53:02 -0800

    Changes to table and view names

--- src/02-pre-processing.sql
+++ src/02-pre-processing.sql
@@ -8,7 +8,7 @@
  */
 
 -- Sample rows of data
-select * from mgdemo.microgrid_data order by building_num, tslocal limit 10;
+SELECT * FROM mgdemo.microgrid_data ORDER BY building_num, tslocal LIMIT 10;
 /*
 building_num |  tslocal   |   usagekw
 --------------+------------+-------------
@@ -32,28 +32,29 @@ Time: 60.157 ms
  * Only data collected every 60 seconds will be used.
  * delta_time represents the time between two consecutive timestamps of a meter.
  */
-CREATE OR REPLACE VIEW mgdemo.mgdata_dt60sec_check_view1 AS
-  SELECT *
-  , ct_tslocal-ct_deltatime_60sec AS diff_ct
+CREATE OR REPLACE VIEW mgdemo.mgdata_deltatime_60sec_check_view AS
+  SELECT
+    *,
+    ct_tslocal-ct_deltatime_60sec AS diff_ct
   FROM (
     SELECT
-      building_num
-      , COUNT(tslocal) AS ct_tslocal
-      , COUNT(difftime) FILTER (WHERE difftime = 60::int) AS ct_deltatime_60sec
+      building_num,
+      COUNT(tslocal) AS ct_tslocal,
+      COUNT(difftime) FILTER (WHERE difftime = 60::int) AS ct_deltatime_60sec
     FROM (
       SELECT
-        building_num
-        , tslocal
-        , tslocal-LAG(tslocal,1,tslocal) OVER (PARTITION BY building_num ORDER BY tslocal) AS difftime
+        building_num,
+        tslocal,
+        tslocal-LAG(tslocal,1,tslocal) OVER (PARTITION BY building_num ORDER BY tslocal) AS difftime
       FROM
         mgdemo.microgrid_data
     ) t1
     GROUP BY
       building_num
   ) t2;
--- Query returned successfully with no result in 36 ms.
+-- Query returned successfully with no result in 50 ms.
 
-select * from mgdemo.mgdata_dt60sec_check_view1 order by building_num;
+SELECT * FROM mgdemo.mgdata_deltatime_60sec_check_view ORDER BY building_num;
 /*
 building_num | ct_tslocal | ct_deltatime_60sec | diff_ct
 --------------+------------+--------------------+---------
@@ -65,78 +66,82 @@ building_num | ct_tslocal | ct_deltatime_60sec | diff_ct
           24 |       1440 |               1439 |       1
           34 |       1440 |               1439 |       1
           35 |       1440 |               1439 |       1
+          36 |       1440 |               1439 |       1
 ...
-Time: 1059.664 ms
 */
 
 -- Count how many buildings have different values for diff_ct
 -- diff_ct should equal 1 for time series with data at 60 second intervals
-select diff_ct, count(*) as ct_buildings
-from mgdemo.mgdata_dt60sec_check_view1
-group by diff_ct
-order by diff_ct;
+SELECT diff_ct, COUNT(*) AS ct_buildings
+FROM mgdemo.mgdata_deltatime_60sec_check_view
+GROUP BY diff_ct
+ORDER BY diff_ct;
 /*
 diff_ct | ct_buildings
 ---------+--------------
       1 |          443
 (1 row)
 
-Time: 1010.711 ms
+Time: 1079.926 ms
 */
 
 /*
  * VIEW with only those meters that have diff_ct = 1. This query gets those meters
  * consistently collected data every 60 seconds.
  */
-CREATE OR REPLACE VIEW mgdemo.mgdata_diffct1_view1 AS
+CREATE OR REPLACE VIEW mgdemo.mgdata_counts_check_join_view AS
 SELECT t1.*
 FROM
-  mgdemo.microgrid_data t1
-  , mgdemo.mgdata_dt60sec_check_view1 t2
+  mgdemo.microgrid_data t1,
+  mgdemo.mgdata_deltatime_60sec_check_view t2
 WHERE
   t1.building_num = t2.building_num
-  AND t2.diff_ct = 1;
--- Query returned successfully with no result in 38 ms.
+  AND
+  t2.diff_ct = 1
+;
+-- Query returned successfully with no result in 36 ms.
 
 -- Note the number of buildings that have diff_ct =1 should match the result
 -- in the query above that counts how many buildings have different diff_ct values
-select count(*) as ct_buildings
-from (
-  select building_num from mgdemo.mgdata_diffct1_view1
-  group by 1
+SELECT COUNT(*) AS ct_buildings
+FROM (
+  SELECT building_num
+  FROM mgdemo.mgdata_counts_check_join_view
+  GROUP BY 1
 ) t1;
 -- ct_buildings
 -- --------------
 --          443
 -- (1 row)
 --
--- Time: 1049.969 ms
+-- Time: 1190.595 ms
+
 
 -- TABLE containing start and end times for each meter and COUNT of how many data points we have.
-CREATE TABLE mgdemo.mgdata_dfct1_tslocalcts_t1 AS
+CREATE TABLE mgdemo.mgdata_counts_and_startend_times_tbl AS
   SELECT *
   FROM (
     SELECT
       building_num,
-      min(tslocal) AS ts_start,
-      max(tslocal) AS ts_end,
+      MIN(tslocal) AS ts_start,
+      MAX(tslocal) AS ts_end,
       COUNT(tslocal) AS ct_tslocal
     FROM
-      mgdemo.mgdata_diffct1_view1
+      mgdemo.mgdata_counts_check_join_view
     GROUP BY
       building_num
   ) t1
-  DISTRIBUTED BY (building_num);
--- Query returned successfully: 443 rows affected, 1164 ms execution time.
+DISTRIBUTED BY (building_num);
+-- Query returned successfully: 443 rows affected, 1237 ms execution time.
 
 -- Check how many meters have different start and end points
 -- or if the number of points included in between is different
 -- It is ideal to have the same start and end points,
 -- and the same number of data points in between
-select ts_start, ts_end, ct_tslocal, count(*) as ct
-from mgdemo.mgdata_dfct1_tslocalcts_t1
-group by 1,2,3
-order by 1,2,3;
+SELECT ts_start, ts_end, ct_tslocal, COUNT(*) AS ct
+FROM mgdemo.mgdata_counts_and_startend_times_tbl
+GROUP BY 1,2,3
+ORDER BY 1,2,3;
 /*
 ts_start  |   ts_end   | ct_tslocal | ct
 ------------+------------+------------+-----
@@ -144,7 +149,7 @@ ts_start  |   ts_end   | ct_tslocal | ct
 1301726040 | 1301788740 |       1046 |   1
 (2 rows)
 
-Time: 25.462 ms
+Time: 35.755 ms
 */
 
 /*
@@ -153,28 +158,63 @@ Time: 25.462 ms
  * and interval COUNT of 1440 (the number of minutes in a 24-hour period).
  * TABLE to be used as starting point for clustering.
  */
-CREATE TABLE mgdemo.mgdata_dfct1_itvlct1440_t1 AS
+CREATE TABLE mgdemo.mgdata_clean_tbl AS
   SELECT t1.*
   FROM
-    mgdemo.mgdata_diffct1_view1 t1
-    , mgdemo.mgdata_dfct1_tslocalcts_t1 t2
+    mgdemo.mgdata_counts_check_join_view t1,
+    mgdemo.mgdata_counts_and_startend_times_tbl t2
   WHERE
     t1.building_num = t2.building_num
   AND t2.ct_tslocal = 1440
 DISTRIBUTED BY (building_num);
--- Query returned successfully: 636480 rows affected, 1780 ms execution time.
+-- Query returned successfully: 636480 rows affected, 2093 ms execution time.
 
 -- This query should show only one ct_ts value (1440 for this dataset)
 -- and the right number of buildings associated with it (442 for this dataset)
-SELECT ct_ts, count(*) as ct_buildings from (
-  select building_num, count(*) as ct_ts from mgdemo.mgdata_dfct1_itvlct1440_t1
-  group by 1
+SELECT ct_ts, COUNT(*) as ct_buildings FROM (
+  SELECT building_num, COUNT(*) AS ct_ts FROM mgdemo.mgdata_clean_tbl
+  GROUP BY 1
 ) t1
-group by 1
-order by 1;
--- ct_ts | ct_buildings 
+GROUP BY 1
+ORDER BY 1;
+-- ct_ts | ct_buildings
 -- -------+--------------
 --  1440 |          442
 -- (1 row)
 --
--- Time: 135.785 ms
+-- Time: 170.593 ms
+
+/*
+Create table as end point of pre-processing and input for array_agg step
+Include contiguous building id, global row id, building-specific row id
+for time series
+*/
+CREATE TABLE mgdemo.mgdata_clean_with_id_tbl AS
+  SELECT
+    rgid,
+    bgid,
+    rid,
+    t1.building_num,
+    tslocal,
+    usagekw
+  FROM (
+    SELECT
+      row_number() over () AS rgid,
+      row_number() over (PARTITION BY building_num) AS rid,
+      *
+    FROM
+      mgdemo.mgdata_clean_tbl
+  ) t1,
+  (
+    SELECT
+      row_number() over () AS bgid,
+      building_num
+    FROM (
+      SELECT building_num
+      FROM mgdemo.mgdata_clean_tbl
+      GROUP BY building_num
+    ) t2
+  ) t3
+  WHERE t1.building_num = t3.building_num
+DISTRIBUTED BY (rgid);
+-- Query returned successfully: 636480 rows affected, 2976 ms execution time.
