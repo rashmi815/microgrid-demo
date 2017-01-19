c616b0ba02ad7b34171986e8f05cab3d05f5aa28
Rashmi Raghu <rraghu@pivotal.io>
2017-01-18 16:22:07 -0800

    clustering code update to include error metrics

--- src/04-clustering.sql
+++ src/04-clustering.sql
@@ -23,22 +23,47 @@ $$
   DECLARE
       sql TEXT;
       numk INT;
+      silh_coef DOUBLE PRECISION;
+      sse DOUBLE PRECISION;
   BEGIN
       sql := 'DROP TABLE IF EXISTS ' || output_kmeans_table_with_schema || ';';
       EXECUTE sql;
+      sql := 'DROP TABLE IF EXISTS ' || output_kmeans_table_with_schema || '_with_clustid;';
+      EXECUTE sql;
+      sql := 'DROP TABLE IF EXISTS ' || output_kmeans_table_with_schema || '_silhcoef;';
+      EXECUTE sql;
+      sql := 'DROP TABLE IF EXISTS ' || output_kmeans_table_with_schema || '_sse;';
+      EXECUTE sql;
 
       sql := 'CREATE TABLE ' || output_kmeans_table_with_schema
-                || ' (centroids DOUBLE PRECISION[][], objective_fn DOUBLE PRECISION,'
+                || ' (k INT, centroids DOUBLE PRECISION[][], objective_fn DOUBLE PRECISION,'
                 || ' frac_reassigned DOUBLE PRECISION, num_iterations INTEGER)'
+                || ' DISTRIBUTED RANDOMLY;';
+      EXECUTE sql;
+
+      sql := 'CREATE TABLE ' || output_kmeans_table_with_schema || '_with_clustid'
+                || ' (k INT, bgid INT, cluster_id INT)'
+                || ' DISTRIBUTED RANDOMLY;';
+      EXECUTE sql;
+
+      sql := 'CREATE TABLE ' || output_kmeans_table_with_schema || '_silhcoef'
+                || ' (k INT, silh_coef DOUBLE PRECISION)'
+                || ' DISTRIBUTED RANDOMLY;';
+      EXECUTE sql;
+
+      sql := 'CREATE TABLE ' || output_kmeans_table_with_schema || '_sse'
+                || ' (k INT, sse DOUBLE PRECISION)'
                 || ' DISTRIBUTED RANDOMLY;';
       EXECUTE sql;
 
       numk := array_upper(k_array,1);
 
       FOR i IN 1..numk LOOP
-          RAISE INFO '===== i = % ==== k = % ===== START ====', i, k_array[i];
+          RAISE INFO '===== i = % ==== k = % ===== START =====', i, k_array[i];
           sql := 'INSERT INTO ' || output_kmeans_table_with_schema
-                  || ' SELECT * '
+                  || ' SELECT'
+                  || ' ' || k_array[i] || ','
+                  || ' *'
                   || ' FROM madlib.kmeanspp('
                   || ' '' ' ||  input_table_with_schema || ''','
                   || ' ''pgram'','
@@ -47,17 +72,64 @@ $$
                   || ' ''madlib.avg'','
                   || ' 100, 0.001);'
                   ;
+          RAISE INFO '===== query =====';
+          RAISE INFO '%', sql;
           EXECUTE sql;
-          RAISE INFO '===== i = % ==== k = % ===== END ====', i, k_array[i];
+
+          RAISE INFO '===== assigning cluster IDs to data points =====';
+          sql := 'INSERT INTO ' || output_kmeans_table_with_schema || '_with_clustid'
+                  || ' SELECT k, bgid, (madlib.closest_column(centroids, pgram)).column_id AS cluster_id'
+                  || ' FROM'
+                  || '   ' || input_table_with_schema || ' AS data,'
+                  || '   (SELECT * FROM ' || output_kmeans_table_with_schema
+                  || '    WHERE k = ' || k_array[i] || ') AS kmeans_out';
+          RAISE INFO '===== query =====';
+          RAISE INFO '%', sql;
+          EXECUTE sql;
+
+          RAISE INFO '===== calculating silhouette_coefficeint =====';
+          sql := 'SELECT madlib.simple_silhouette'
+                        || ' (''' || input_table_with_schema || ''','
+                        || ' ''pgram'','
+                        || ' (SELECT centroids FROM ' || output_kmeans_table_with_schema || ' where k=' || k_array[i] || '),'
+                        || ' ''madlib.dist_norm2'''
+                        || ' );';
+          RAISE INFO '===== query =====';
+          RAISE INFO '%', sql;
+          EXECUTE sql INTO silh_coef;
+          RAISE INFO '===== silhouette_coefficeint=% =====', silh_coef;
+
+          sql := 'INSERT INTO ' || output_kmeans_table_with_schema || '_silhcoef'
+                  || ' VALUES (' || k_array[i] || ', ' || silh_coef || ');';
+          RAISE INFO '===== query =====';
+          RAISE INFO '%', sql;
+          EXECUTE sql;
+
+          RAISE INFO '===== calculating sum of squared errors (sse) =====';
+          sql := 'INSERT INTO ' || output_kmeans_table_with_schema || '_sse'
+                  || ' SELECT k, bgid, data.pgram - '
+                  || ' FROM'
+                  || ' ' || input_table_with_schema || ' as data'
+                  || ' ' || output_kmeans_table_with_schema || '_with_clustid as clust_id_tbl,'
+                  || '   (SELECT * FROM ' || output_kmeans_table_with_schema
+                  || '    WHERE k = ' || k_array[i] || ') AS kmeans_out'
+                  || ' WHERE data.bgid = clust_id_tbl.bigid'
+                  || ' GROUP BY cluster_id';
+          RAISE INFO '===== query =====';
+          RAISE INFO '%', sql;
+          EXECUTE sql INTO sse;
+          RAISE INFO '===== sse=% =====', sse;
+
+          RAISE INFO '===== i = % ==== k = % ===== END =====', i, k_array[i];
       END LOOP;
   END;
 $$
 LANGUAGE 'plpgsql';
--- Query returned successfully with no result in 33 ms.
+--
 
 -- Run the function for 3 to 20 clusters
 SELECT mgdemo.run_kmeans('mgdemo.mgdata_pgram_array_tbl','mgdemo.mgdata_kmeans_out',array_agg(km order by km))
-FROM (SELECT generate_series(3,20,1) as km) t1;
+FROM (SELECT generate_series(3,5,1) as km) t1;
 -- Total query runtime: 13873 ms.
 -- 1 row retrieved.
 
@@ -76,6 +148,42 @@ FROM (SELECT generate_series(3,20,1) as km) t1;
       )
   ;
 --
+
+
+/*
+-- template on how to unnest centroids table that is constructed in the plpgsql function
+select k, ucid, array_agg(ucentorder order by ucentorder) as centorder, array_agg(ucent order by ucentorder) as cent from
+(
+select t2.k, unnest(arr_ucid) as ucid, unnest(arr_centorder) as ucentorder, unnest(cent) as ucent from
+(
+select k, array_agg(ucidmult order by ucidmult,centorder) as arr_ucid, array_agg(centorder order by ucidmult,centorder) as arr_centorder from
+(
+select k, ucidmult, row_number() over (partition by k, ucidmult) as centorder from
+(
+select k, unnest(cidmult) as ucidmult from
+(
+select k, cid, madlib.array_fill(madlib.array_of_bigint(2)::int[],cid::int) as cidmult from
+(
+select k, generate_series(0,k-1,1) as cid from (select generate_series(2,4,1) as k) t0
+) ta
+) tb
+) tc
+) td
+group by k
+--order by k
+) t2,
+(
+-- replace with the centroids table from plpgsql output
+select * from (select 2 as k, array[array[1,2],array[9,10]] as cent union all select 3, array[array[1,2],array[9,10],array[21,22]]
+union all select 4, array[array[1,2],array[9,10],array[21,22],array[33,34]]) q3
+) t3
+where t2.k = t3.k
+--order by t2.k, ucid, ucentorder
+) t4
+group by k, ucid
+order by k, ucid;
+*/
+
 
 
 /*
